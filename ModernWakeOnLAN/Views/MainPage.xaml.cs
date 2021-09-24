f4b9958d70d8506fd57a9ba00aef501c432f98f9
Sam <samuelg12321@hotmail.com>
2021-09-24 18:05:07 +1200

    Add project files.

---
+++ ModernWakeOnLAN/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using ModernWakeOnLAN.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace ModernWakeOnLAN.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
